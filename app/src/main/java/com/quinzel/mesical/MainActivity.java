5a58d94bbb6eb18486659af515c25545cd0789d2
runan <rrrafon@gmail.com>
2018-11-16 00:34:00 +1300

    layout fine

---
+++ app/src/main/java/com/quinzel/mesical/MainActivity.java
@@ -0,0 +1,39 @@
+package com.quinzel.mesical;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    /* this declares all the button in main Page*/
+    private Button bRender = null;
+    private Button bRecord = null;
+    private Button bMyFiles = null;
+    private Button bQuit = null;
+    private Button bSettings = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bRender =  findViewById(R.id.buttonMesical);
+        bRecord =  findViewById(R.id.buttonMainRecord);
+        bMyFiles =  findViewById(R.id.buttonMainMyVideos);
+        bQuit =  findViewById(R.id.buttonMainQuit);
+        bSettings =  findViewById(R.id.buttonMainSettings);
+
+        bRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, record.class );
+                startActivity(i);
+            }
+
+        });
+    }
+
+}
--- app/src/main/java/com/quinzel/mesical/record.java
+++ app/src/main/java/com/quinzel/mesical/record.java
@@ -1,13 +1,49 @@
 package com.quinzel.mesical;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class record extends AppCompatActivity {
+    private final int REQUEST_VIDEO_CAPTURE = 1;
+    Uri vidLoc = null;
+
+    private Button bCamera = null;
+    private Button bMyFiles = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        bCamera = findViewById(R.id.buttonRecordCamera);
+        bMyFiles = findViewById(R.id.buttonRecordFiles);
+
+        bCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+
+                    Uri vidLoc = takeVideoIntent.getData();
+                }
+            }
+        });
+        /*
+        bMyFiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mVideoView.setVideoURI(videoUri);
+            }
+        });
+        */
+
+
+
     }
 }
